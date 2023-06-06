a3776d148b425c8245d53078fc554e4dba7196f5
Northfear <11291116+Northfear@users.noreply.github.com>
2023-06-06 22:23:13 +0300

    Fix direct touch controls with non-fullscreen resolutions

--- src/touch.cc
+++ src/touch.cc
@@ -292,8 +292,17 @@ void touch_process_gesture()
             if ((touches[active[0]].touchId == 0 && frontTouchpadMode == TouchpadMode::kTouchDirect)
                 || (touches[active[0]].touchId == 1 && rearTouchpadMode == TouchpadMode::kTouchDirect))
             {
+                SDL_Rect renderRect = getRenderRect();
+                float width = static_cast<float>(screenGetWidth());
+                float height = static_cast<float>(screenGetHeight());
+
+                int touchPosX = static_cast<float>(VITA_FULLSCREEN_WIDTH * (currentCentroid.x / width) - renderRect.x) *
+                                            (width / renderRect.w);
+                int touchPosY = static_cast<float>(VITA_FULLSCREEN_HEIGHT * (currentCentroid.y / height) - renderRect.y) *
+                                            (height / renderRect.h);
+
                 mouseHideCursor();
-                _mouse_set_position(currentCentroid.x, currentCentroid.y);
+                _mouse_set_position(touchPosX, touchPosY);
                 mouseShowCursor();
             }
 #endif
